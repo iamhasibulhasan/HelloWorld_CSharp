46c8834aeba6e99b1e1a226bcc2b1565f774e650
iamhasibulhasan <hashibul218@gmail.com>
2024-01-02 21:50:46 +0600

    type casting

--- HelloWorld_CSharp/Program.cs
+++ HelloWorld_CSharp/Program.cs
@@ -7,3 +7,29 @@ char d = 'a';
 double d2 = 20.125425;
 float d3 = 0.21545f;
 decimal d4 = 25;
+
+// type casting
+
+/*
+ * 2 types of type casting
+ * implicit
+ * explicit
+ */
+
+// explicit casting
+double myNum  = 1520.5;
+int ok = (int)myNum;
+
+// advance string to int
+
+string s1 = "4585485";
+int result  = 0;
+
+if (int.TryParse(s1, out result))
+{ 
+    Console.WriteLine("Success = "+ result);
+}
+else
+{
+    Console.WriteLine("Faliure = "+ result);
+}
